25fb6e9cfff4f8bd2f4589cbf16c630c3bce8d29
kulen9999 <Kulen9000@gmail.com>
2026-04-09 15:00:01 +0200

    website template made

---
+++ my-app/src/page.jsx
@@ -0,0 +1,13 @@
+import "./Page.css";
+
+export default function Page() {
+  return (
+    <div className="page">
+      <div className="frame">
+        <div className="inner-frame">
+          {/* Intentionally blank for now */}
+        </div>
+      </div>
+    </div>
+  );
+}
