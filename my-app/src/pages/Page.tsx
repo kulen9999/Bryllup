58bb1651cf8f6ff68c9ebdb8ce1eacb3129947a8
Daniel Høyland <daniehoy@stud.ntnu.no>
2026-04-13 00:53:57 +0200

    added icons

--- my-app/src/pages/Page.tsx
+++ my-app/src/pages/Page.tsx
@@ -1,5 +1,14 @@
 import { useState } from "react";
 import "./Page.css";
+import titel from "../assets/Tittel.png"
+import kake from "../assets/Kake.png"
+import kirke from "../assets/Kirke.png"
+import middag from "../assets/Middag.png"
+import slutt from "../assets/Slutt.png"
+import door from "../assets/door.png"
+import blomsttitel from "../assets/blomst.png"
+
+
 
 type Tab = "program" | "meny" | "bordkart";
 
@@ -12,7 +21,7 @@ export default function Page() {
         <div className="inner-frame">
           
           <div className="card">
-            
+             <img src={titel}></img>
             {/* Top divider */}
             <div className="divider" />
 
@@ -44,24 +53,26 @@ export default function Page() {
 
 function Program() {
   const items = [
-    ["12.30", "Vielse"],
-    ["13.45", "Brudeparet kjører videre"],
-    ["15.00", "Lokalet åpner for gjestene"],
-    ["16.00", "Brudeparet ankommer"],
-    ["16.30", "Middag"],
-    ["18.30", "Pause og mingling"],
-    ["19.00", "Kaffe og kaker"],
-    ["22.00", "Brudevals og dans"],
-    ["23.00/24.00", "Farvel til brudeparet"],
+    ["12.30", "Vielse", kirke, 32],
+    ["13.45", "Brudeparet kjører videre", slutt, 16],
+    ["15.00", "Lokalet åpner på vaulali ", door, 32],
+    ["16.00", "Brudeparet ankommer", slutt, 16],
+    ["16.30", "Middag", middag, 32],
+    ["19.00", "Kaffe og kaker", kake, 32],
+    ["23.30", "Farvel til brudeparet", slutt, 16],
   ];
 
   return (
     <div>
       <h1>Program</h1>
-      {items.map(([time, text], i) => (
+      {items.map(([time, text, icon, size], i) => (
         <div key={i} className="row">
           <span>{time}</span>
           <span>{text}</span>
+          {icon!=null && ( 
+            <span><img src={icon} height={size}/></span>
+          )}
+          
         </div>
       ))}
     </div>
